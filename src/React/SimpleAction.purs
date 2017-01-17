da3ffecbbeda47e1c18716c801de764a49d80bdc
Jolse Maginnis <doolse@gmail.com>
2017-01-17 15:07:42 +1100

    Extra dispatchers

--- src/React/SimpleAction.purs
+++ src/React/SimpleAction.purs
@@ -5,10 +5,9 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (class MonadEff, liftEff)
 import Control.Monad.Reader (ReaderT(ReaderT))
 import Data.Function.Uncurried (Fn4, runFn4)
-import React (Disallowed, GetInitialState, ReactElement, ReactProps, ReactRefs, ReactSpec, ReactState, ReactThis, Read, ReadOnly, ReadWrite, Refs, Render, readState, spec', transformState)
+import React (GetInitialState, ReactClass, ReactElement, ReactProps, ReactRefs, ReactSpec, ReactState, ReactThis, Read, ReadOnly, ReadWrite, Refs, Render, createClass, readState, spec', transformState)
 import React (getProps, getRefs) as R
 import React.SimpleAction.Dispatch (class Dispatchable, class FromContext, State(..), dispatch, fromContext)
-
 
 type ReactReaderT props state m a = ReaderT (ReactThis props state) m a
 
@@ -40,15 +39,6 @@ unsafeWithRef f s = do
 emptyHandler :: forall a m. (Applicative m) => a -> m Unit
 emptyHandler = const $ pure unit
 
-type RenderEff eff = Eff (props::ReactProps, refs::ReactRefs Disallowed, state :: ReactState ReadOnly | eff) ReactElement
-type CallbackEff eff a = Eff ( props :: ReactProps, state :: ReactState ReadWrite, refs :: ReactRefs ReadOnly | eff) a
-type InitialStateEff eff state= Eff ( props :: ReactProps, state :: ReactState Disallowed, refs :: ReactRefs Disallowed | eff) state
-
-
-didMount :: forall eval props state action eff. Dispatchable eval (ReactThis props state) action (CallbackEff eff Unit) => action -> eval -> ReactSpec props state eff -> ReactSpec props state eff
-didMount a e = _ { componentDidMount = flip (dispatch e) a }
-
-
 class ReactSpecCreator eval initialstate render props state eff | eval initialstate render -> props state eff where
   createSpec :: eval -> initialstate -> (state -> render) -> ReactSpec props state eff
 
@@ -58,30 +48,48 @@ class ReactInitialState eval initialstate props state eff | initialstate -> stat
 class ReactRender eval renderer props state eff where
   createRenderer :: eval -> renderer -> Render props state eff
 
-instance dftRSC :: (
-    ReactInitialState eval initialstate props state eff
-  , ReactRender eval renderer props state eff)
+createComponent :: forall props state eval renderer eff. ReactSpecCreator eval (State state) renderer props state eff
+  => state -> (state -> renderer) -> eval -> ReactClass props
+createComponent = createLifecycleComponent (const $ id :: ReactSpec props state eff -> ReactSpec props state eff)
+
+createLifecycleComponent :: forall props state eval renderer eff. ReactSpecCreator eval (State state) renderer props state eff
+  => (eval -> ReactSpec props state eff -> ReactSpec props state eff) ->
+   state -> (state -> renderer) -> eval -> ReactClass props
+createLifecycleComponent specf state = createComponent' specf (State state)
+
+createComponent' :: forall props state initialstate eval renderer eff. ReactSpecCreator eval initialstate renderer props state eff
+  => (eval -> ReactSpec props state eff -> ReactSpec props state eff) ->
+   initialstate -> (state -> renderer) -> eval -> ReactClass props
+createComponent' f state renderer eval = createClass $ f eval $ createSpec eval state renderer
+
+didMount :: forall eval props state action eff. Dispatchable eval (ReactThis props state) action (Eff ( props :: ReactProps, state :: ReactState ReadWrite, refs :: ReactRefs ReadOnly | eff) Unit)
+  => action -> eval -> ReactSpec props state eff -> ReactSpec props state eff
+didMount a e = _ { componentDidMount = flip (dispatch e) a }
+
+willUnmount :: forall eval props state action eff. Dispatchable eval (ReactThis props state) action (Eff ( props :: ReactProps, state :: ReactState ReadOnly, refs :: ReactRefs ReadOnly | eff) Unit)
+  => action -> eval -> ReactSpec props state eff -> ReactSpec props state eff
+willUnmount a e = _ { componentWillUnmount = flip (dispatch e) a }
+
+instance defaultCreateSpec :: (ReactInitialState eval initialstate props state eff, ReactRender eval renderer props state eff)
   => ReactSpecCreator eval initialstate renderer props state eff where
   createSpec eval initial renderer = spec' (createInitialState eval initial) \this -> do
     s <- readState this
     (createRenderer eval $ renderer s) this
 
-instance iss :: -- TypeEquals state2 state =>
-  ReactInitialState eval (State state) props state eff where
+instance stateGetInitialState :: ReactInitialState eval (State state) props state eff where
   createInitialState eval (State s) = const $ pure s
 
-instance dftIS :: (
-  FromContext eval (ReactThis props state) r Eff (props :: ReactProps, refs::ReactRefs (), state::ReactState ()|eff)
+instance fromContextIS :: (FromContext eval (ReactThis props state) r Eff (props :: ReactProps, refs::ReactRefs (), state::ReactState ()|eff)
   , ReactInitialState eval next props state eff)
   => ReactInitialState eval (r -> next) props state eff where
   createInitialState eval initial = \this -> do
     r <- fromContext eval this
     createInitialState eval (initial r) this
 
-instance renderElem :: ReactRender eval ReactElement props state eff where
+instance renderReactElem :: ReactRender eval ReactElement props state eff where
   createRenderer _ = const <<< pure
 
-instance dftRR :: (
+instance fromContextRR :: (
     ReactRender eval next props state eff
   , FromContext eval (ReactThis props state) r Eff (props :: ReactProps, refs::ReactRefs (), state::ReactState (read::Read)|eff)
   )
--- src/React/SimpleAction/Dispatch.purs
+++ src/React/SimpleAction/Dispatch.purs
@@ -5,13 +5,10 @@ import Control.Monad.Aff (Aff, launchAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
 import Control.Monad.Reader (ReaderT, runReaderT)
-import Data.Function.Eff (EffFn1, mkEffFn1)
+import Data.Function.Eff (EffFn1, EffFn2, EffFn3, mkEffFn1, mkEffFn2, mkEffFn3)
 import Data.Maybe (Maybe, maybe)
 import React (ReactProps, ReactState, ReactThis, Read, getProps, readState)
 import Type.Equality (class TypeEquals, to)
-
-class FromContext eval context a m (eff :: # !) | eval context a m -> eff where
-  fromContext :: eval -> context -> m eff a
 
 class Dispatchable eval context action result | eval -> context where
   dispatch :: eval -> context -> action -> result
@@ -21,9 +18,6 @@ instance funcMaybeDispatch :: (Applicative m, Dispatchable Unit context dispatch
 
 instance funcDispatch :: Dispatchable Unit context m result => Dispatchable (action -> m) context action result where
   dispatch f c a = dispatch unit c (f a)
-
--- instance dispatchToContextFunc :: Applicative m => Dispatchable eval context a (context -> m a) where
---   dispatch _ _ a = const $ pure a
 
 instance readerTTest :: Dispatchable eval context (m a) result => Dispatchable eval context (ReaderT context m a) result where
   dispatch e c a = dispatch e c $ runReaderT a c
@@ -44,15 +38,25 @@ newtype DispatchEff a = DispatchEff (forall ev eff. (ev -> a) -> ev -> Eff eff U
 
 newtype DispatchEffFn a = DispatchEffFn (forall ev eff. (ev -> a) -> EffFn1 eff ev Unit)
 
-instance dispatchEffD :: (Applicative (m eff), Dispatchable eval context action (Eff eff Unit)) =>
-  FromContext eval context (DispatchEff action) m eff where
+newtype DispatchEffFn2 a = DispatchEffFn2 (forall ev ev2 eff. (ev -> ev2 -> a) -> EffFn2 eff ev ev2 Unit)
+
+newtype DispatchEffFn3 a = DispatchEffFn3 (forall ev ev2 ev3 eff. (ev -> ev2 -> ev3 -> a) -> EffFn3 eff ev ev2 ev3 Unit)
+
+instance dispatchEffD :: (Applicative (m eff), Dispatchable eval context action (Eff eff Unit))
+  => FromContext eval context (DispatchEff action) m eff where
   fromContext eval c = pure $ DispatchEff (\handle ev -> unsafeCoerceEff $ (dispatch eval c (handle ev)) :: Eff eff Unit)
 
-instance dispatchEffFnD :: (Dispatchable eval context action (Eff eff Unit), Dispatchable eval context next result) => Dispatchable eval context (DispatchEffFn action -> next) result where
-  dispatch eval c f =  dispatch eval c $ f $ mkDispatchEffFn (dispatch eval c :: action -> Eff eff Unit)
+instance dispatchEffFnD :: (Applicative (m eff), Dispatchable eval context action (Eff eff Unit))
+  => FromContext eval context (DispatchEffFn action) m eff where
+  fromContext eval c = pure $ DispatchEffFn (\handle -> mkEffFn1 \ev -> unsafeCoerceEff $ (dispatch eval c (handle ev)) :: Eff eff Unit)
 
-mkDispatchEffFn :: forall action eff. (action -> Eff eff Unit) -> DispatchEffFn action
-mkDispatchEffFn f = DispatchEffFn (\handle -> mkEffFn1 \ev -> unsafeCoerceEff $ f (handle ev))
+instance dispatchEffFn2D :: (Applicative (m eff), Dispatchable eval context action (Eff eff Unit))
+  => FromContext eval context (DispatchEffFn2 action) m eff where
+  fromContext eval c = pure $ DispatchEffFn2 (\handle -> mkEffFn2 \ev ev2 -> unsafeCoerceEff $ (dispatch eval c (handle ev ev2)) :: Eff eff Unit)
+
+instance dispatchEffFn3D :: (Applicative (m eff), Dispatchable eval context action (Eff eff Unit))
+  => FromContext eval context (DispatchEffFn3 action) m eff where
+  fromContext eval c = pure $ DispatchEffFn3 (\handle -> mkEffFn3 \ev ev2 ev3 -> unsafeCoerceEff $ (dispatch eval c (handle ev ev2 ev3)) :: Eff eff Unit)
 
 dispatchEff :: forall ev action eff. DispatchEff action -> (ev -> action) -> ev -> Eff eff Unit
 dispatchEff (DispatchEff d) = d
@@ -69,8 +73,14 @@ dispatchEffFn_ (DispatchEffFn d) a = d $ const a
 newtype State s = State s
 newtype Props p = Props p
 
+class FromContext eval context a m (eff :: # !) | eval context a m -> eff where
+  fromContext :: eval -> context -> m eff a
+
 instance stateFromContext :: TypeEquals state state2 => FromContext eval (ReactThis props state) (State state2) Eff (state::ReactState (read::Read)|eff) where
-  fromContext eval this = State <<< to <$> readState this
+  fromContext _ this = State <<< to <$> readState this
 
 instance propsFromContext :: TypeEquals props props2 => FromContext eval (ReactThis props state) (Props props2) Eff (props::ReactProps|eff) where
-  fromContext eval this = Props <<< to <$> getProps this
+  fromContext _ this = Props <<< to <$> getProps this
+
+instance thisFromContext :: (TypeEquals (ReactThis props state) (ReactThis props2 state2), Applicative (m eff)) => FromContext eval (ReactThis props state) (ReactThis props2 state2) m eff where
+  fromContext _ this = pure $ to this
